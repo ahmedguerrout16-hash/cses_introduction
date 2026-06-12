528f1ed9df4aa3526d367da2b59b8616685ee846
ahmedguerrout16-hash <ahmedguerrout16@gmail.com>
2026-06-13 01:44:40 +0700

    Update traillling_zeros.cpp
    Refactored code to calculate trailing zeros in a factorial using a more efficient method.

--- traillling_zeros.cpp
+++ traillling_zeros.cpp
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 int main()
 {
+    cin.tie(0);
+    
     int num;cin>>num;
-    int poww2=pow(2,floor(log2(num)));
-    int poww5=pow(5,floor(log(num)/log(5)));
-    int num2=0,num5=0;
-    for(int i=2;i<=num;i+=2)
-    {
-        num2=num2+log2(gcd(i,poww2));\
-    }
-      for(int i=5;i<=num;i+=5)
-    {
-        num5=num5+(log(gcd(i,poww5))/log(5));
-    }
-    cout<<min(num2,num5);
+    int num5=0;
+    ll j=5;
+    while((num/j)!=0)
+     {
+       num5+=(num/j);j=j*5;
+     }
+    cout<<num5;
+
 }
